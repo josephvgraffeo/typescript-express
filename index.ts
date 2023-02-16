eb9a42de59d198723f267fe6b8c021d0855f26bf
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-16 14:39:37 -0500

    basic express setup using typescript

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("hello typescript")
+})
+
+app.listen(3000, () => { console.log("Hooray🥶") })
